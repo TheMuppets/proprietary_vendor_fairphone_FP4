6ae750e1da3073a402c75045473c6cd247c9a9f5
Michael Bestas <mkbestas@lineageos.org>
2024-11-03 07:41:57 +0200

    FP4: Drop libril-qcril-hook-oem.so
    Change-Id: Ibf025067e53207acd69ed6305a714877bc077608

--- FP4-vendor.mk
+++ FP4-vendor.mk
@@ -640,7 +640,6 @@ PRODUCT_PACKAGES += \
     libril-qc-logger \
     libril-qc-ltedirectdisc \
     libril-qc-radioconfig \
-    libril-qcril-hook-oem \
     librilqmiservices \
     librpmb \
     libsdm-color \
